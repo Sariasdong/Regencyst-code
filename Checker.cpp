8058bdbf9ab0c14eeb6b5733b057d4aa5c1e1331
Sariasdong <nickthompson1764@yahoo.com>
2016-03-20 20:51:58 -0700

    Added files via upload
    Guess int 1-10 game

---
+++ Checker.cpp
@@ -0,0 +1,10 @@
+#include <Checker.h>
+#include <iostream>
+using namespace std;
+
+Checker::Checker()
+{
+	cerr << "The character you typed is not a valid number" << endl;
+	cerr << "Please select a number from 1-10" << endl;
+
+}
--- Source.cpp
+++ Source.cpp
@@ -1,63 +1,31 @@
-#include "test.h"
-#include <iostream>
+#include <test.h>
+#include <iostream> 
+#include <windows.h>
 #include <string>
-#include <stdio.h> 
-#include <stdlib.h>    
-#include <time.h>
-#include <Checker.h>
-
-
 using namespace std;
-test::test()
-{
-	int guess(0);
-	int iSecret;
-	int wrong(0);
-	int pause(0);
-	const int MAXMISTAKES = 5;
+
+int main() {
+
+	HANDLE outcon = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	CONSOLE_FONT_INFOEX font;
+	GetCurrentConsoleFontEx(outcon, false, &font);
+	font.dwFontSize.X = 7;
+	font.dwFontSize.Y = 12;
+	SetCurrentConsoleFontEx(outcon, false, &font);
+	SetConsoleTextAttribute(outcon, 0X03); //changes consol color
+
+
 	char ans;
-	int min_value = 1;
-	int max_value = 10;
 
-	srand(time(NULL));	//initilizes random number
-	iSecret = rand() % 10 + 1; //defines secret number
+	do {
+			test bo;
 
-	printf("Guess a number between (1-10):\n");
-
-	while ((wrong < MAXMISTAKES && guess != iSecret)) {
-		cin >> guess;
-
-		if ((guess >= min_value) && (guess <= max_value));
-
-		else {
-			cout << guess;
-			Checker bo;
-			cin.clear();
-			cin.ignore(1000, '\n');
-		}
+			cout << "Please enter y/n" << endl;
+			cin >> ans;
+		} while ((ans == 'y') || (ans == 'Y'));
 
 
-		if (guess < iSecret & guess >= 1) {
-			cout << "Your guess was low" << endl;
-			++wrong;
-		}
 
-		if (guess > iSecret) {
-			cout << "Your guess was too high" << endl;
-			++wrong;
-		}
-
-
-		if (guess == iSecret) {
-			cout << "Congrats, you're a winner" << endl;
-			cout << "Would you like to try again?" << endl;
-
-		}
-
-		if (wrong == MAXMISTAKES) {
-
-			cout << "Sorry, you're a loser?" << endl;
-			cout << "Would you like to try again?" << endl;
-		}
+		return 0;
 	}
-}
